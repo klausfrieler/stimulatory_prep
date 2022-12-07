26328645925f9e2ef59e85b43ee4ed50efac7269
klaus <kgf@omniversum.de>
2022-12-08 00:47:10 +0100

    Intermediate commit

--- stimulatory_prep_survey.R
+++ stimulatory_prep_survey.R
@@ -1,115 +1,6 @@
 library(tidyverse)
 library(psychTestR)
-
-problems_info = list(
-  de = shiny::tags$span("Probleme? Kontaktieren Sie klaus.frieler@ae.mpg.de mit einem Link zu dieser Seite.", 
-                        style = "color:#999999",
-  ),
-  de_f = shiny::tags$span("Probleme? Kontaktieren Sie klaus.frieler@ae.mpg.de mit einem Link zu dieser Seite.", 
-                          style = "color:#999999",  
-  ),
-  en = shiny::tags$span("Problems? Contact klaus.frieler@ae.mpg.de with a link to this page.", 
-                        style = "color:#999999",
-                        shiny::tags$br(),
-  )
-)
-
-likert_items <- c()
-
-free_text_items <- list(
-  name = list(
-    prompt = "Please give a succinct and descriptive name for your stimulus data set"
-  ),
-  pub_ref = list(
-    prompt = "Please enter the DOI of a publication, where the stimulus set was used.",
-    subprompt = "If you don't have a DOI, please provide bibliographical information (in any style)."
-  ),
-  location = list(
-    prompt = "Please enter a link or URL, where the stimulus set can be accessed.", 
-    subprompt = "Does not need to be publicly available."),
-  description = list(
-    prompt = "Please describe the stimulus data set shortly."
-  ),
-  feedback = list(
-    prompt = "Would you like to give some feedback on the survey or on the project itself?"
-  )
-)
-
-numeric_input_items <- list(
-  subsets = list(
-    prompt = "How many proper subsets does the stimulus set contain?"
-    ),
-    entities = list(
-      prompt = "How many files does the stimulus set contain?"
-    )
-)
-
-dropbox_items <- list(
-  source = list(
-    prompt = "What is the origin of the main material in the stimulus set?",
-    subprompt = shiny::span("Note: 'Internal' means created by researcher or associates; qualifier 'Experiments' means that stimuli were created in a production experiment.", style = "font-size:small"),
-    items = c("External/Commercial", 
-              "External/Free",
-              "External/Experiment", 
-              "Internal/Researcher",
-              "Internal/Experiment", 
-              "Mixed", 
-              "Other")),
-  
-  modification = list(
-    prompt = "For external material, how many modifications have been made?",
-    items = c("None", "Some", "Substantial", "Mixed", "Only internal material" )
-  ),
-  role = list(
-    prompt= "What is your professional role?", 
-    items = c(
-      "PhD",
-      "Post-Doc",
-      "Senior Researcher", 
-      "Research Group Leader",
-      "Director", 
-      "Other"))
-)
-
-checkbox_items <- list(
-  scientific_field = list(
-    prompt = "In which scientific fields  was the stimulus set used so far?",
-    subprompt = "Multiple answers possible",
-    items = c("Psychology", "Neuroscience", "Musicology",
-              "Literary science", "(Psycho-)Linguistics",
-              "Media science", "Sociology", "Genetics", 
-              "Other")
-  ),
-  psych_topic = list(
-    prompt = "For which psychological or neuroscience topic was the stimulus set used so far?",
-    subprompt = "Multiple answers possible",
-    items = c("Non-psychological", 
-              "Aesthetics", "Emotion", "Perception", "Cognition", "Memory", "Production", "Physiology", 
-              "Brain morphology",
-              "Other")
-  ),
-  art_domain = list(
-    prompt = "To which aesthetic or artistic domain does you stimulus set main pertain?",
-    subprompt = "Multiple answers possible",
-    items = c("Non-aesthetics", "Music", "Literature", 
-              "Non-fiction Text", "Visual Arts", 
-              "Movies", "Music Videos", "Dance", "Theatre", 
-              "Performance", "Other")
-  ),
-  paradigm = list(
-    prompt = "For which experimental paradigm was the stimulus set mainly designed and/or used for?",
-    subprompt = "Multiple answers possible",
-    items = c("Oddball", "Comparison", "Reaction time", "Judgement/Rating", "fMRI", "EEG", "Eye-tracking", "MEG", 
-                "tDCM", "Physiology", "Other")
-  ),
-  media_types = list(
-    prompt = "Please select all media types contained in the stimulus set",
-    items = c("Text","Picture", "Video/Live", "Video/Animated", "Audio/Music", "Audio/Speech",
-      "Audio/Sounds", "Audio/Mixed", "VR/AR", "Other")
-  )
-  
-)
-likert_options <- c("Strongly disagree", "Disagree", "Neither disagree nor agree", "Agree", "Strongly agree") 
+source("stimulatory_survey_resources.R")
 
 make_likert_pages <- function(items = NULL){
   if(is.null(items)){
@@ -198,10 +89,73 @@ make_free_text_pages <- function(items = NULL){
       label = sprintf("text_%s", i),
       prompt = prompt, 
       button_text = "Continue",
+      width = "600px",
       one_line = FALSE)
     
   })  
 }
+
+make_person_page <- function(label = "person_page",  id = 1, prompt = "Please enter your personal data", width = 400){
+  validate <- NULL
+  on_complete <- NULL
+  admin_ui <- NULL
+  save_answer <- TRUE
+  button_text <- "Continue"
+  first_name <- shiny::textInput("first_name", label = "First Name", placeholder = "",  width = width)
+  middle_name <- shiny::textInput("middle_name", label = "Middle Name", placeholder = "",  width = width)
+  last_name <- shiny::textInput("last_name", label = "Last Name", placeholder = "",  width = width)
+  orcid_id   <- shiny::textInput("orcid", label = "ORCID", placeholder = "",  width = width)
+  department <- shiny::selectInput("department", label = "Department", 
+                                   choice = c("Music", 
+                                              "Neuroscience", 
+                                              "Literature", 
+                                              "Cognitive Neuroscience", 
+                                              "Not at MPIAE",
+                                              "Other"),
+                                   selected = "Other", 
+                                   width = 200)
+  prof_role <- shiny::selectInput("prof_role", "Professional Role", 
+                                  choices = professional_roles,
+                                  selected = "Other", 
+                                  width = 200)  
+  get_answer <- function(input, state, ...) {
+    answer <- input %>% 
+      reactiveValuesToList() %>% 
+      as_tibble() %>% 
+      select(all_of(c("last_name","middle_name", "first_name", "orcid", "department")))
+    
+    page_counter <- psychTestR::get_local(sprintf("%s_page_counter", label), state)
+    answer <- answer %>% mutate(counter = page_counter, type = label) 
+    psychTestR::set_local(sprintf("%s_page_counter", label), as.integer(page_counter) + 1 , state)
+    
+    psychTestR::set_local("repeat", identical(input$last_btn_pressed, "repeat"), state)
+    print(sprintf("Repeat = %s", identical(input$last_btn_pressed, "repeat")))
+    print(answer)
+    answer
+  }
+  validate <- function(input, ...){
+    if(nzchar(input$first_name) && nzchar(input$last_name)) {
+      TRUE
+    }
+    else{
+      FALSE
+    }
+  }
+  prompt <- shiny::h4(prompt)
+  body = shiny::div(onload = "document.getElementById('first_name').value = '';", 
+                    psychTestR:::tagify(prompt), 
+                    first_name, 
+                    middle_name, 
+                    last_name, 
+                    orcid_id, 
+                    department, 
+                    prof_role)
+  ui <- shiny::div(body, shiny::div(trigger_button("repeat", "Another entry"), trigger_button("next", button_text)))
+  page(ui = ui, label = sprintf("%s%s", label, id), get_answer = get_answer, save_answer = save_answer, 
+       validate = validate, on_complete = on_complete, final = FALSE, 
+       admin_ui = admin_ui)      
+}
+
 make_numeric_input_pages <- function(items = NULL){
   if(is.null(items)){
     items <- numeric_input_items
@@ -227,25 +181,8 @@ make_numeric_input_pages <- function(items = NULL){
   
 }
 
-intro_page <- function(){
-  psychTestR::one_button_page(
-    body = shiny::div(
-      shiny::h3("Dear colleagues,", style = "text-align:left"),
-      shiny::p("we like to gather some information on stimulus sets you were using in your past research."),
-      shiny::p("This is a pre-screening survey to get an impression on what kind of stimulus data sets are out there,", 
-               "Filling in the survey will help the Stimulus Database Working Group (SDB-WG) to set up a system", 
-               "allowing researcher the easy re-use of stimuli while serving as a standardized archive and documenting site for stimulus sets at the same time."),
-      shiny::p("You can also enter more than one stimulus set using the link at the end of survey"),
-      shiny::p("Best wishes,", shiny::tags$br(), 
-               "Klaus Frieler", shiny::tags$br(), 
-               "(on behalf of the SDB-WG)", style = "font-style:italic"),
-      style = "margin-left:20%;margin-right:20%;text-align: justify"
-      
-      ),
-    button_text = "Continue"
-  )
-  
-}
+
+
 stimulatory_survey  <- function(title = "MPIAE Stimulus Sets Survey",
                                 documentation = "MSSS",
                                 admin_password = "conifer",
@@ -256,17 +193,41 @@ stimulatory_survey  <- function(title = "MPIAE Stimulus Sets Survey",
                                 ...) {
   elts <- psychTestR::join(
     intro_page(),
-    make_free_text_pages(c("name", "pub_ref", "location")),
-    make_numeric_input_pages(c("subsets", "entities")),
-    make_free_text_pages(c("description")),
-    make_checkbox_pages(),
+    psychTestR::get_p_id(prompt = p_id_prompt),
     make_dropbox_pages(),
+    make_free_text_pages(c("general_description", "design_spec", "naming_scheme")),
+    make_numeric_input_pages(c("entities")),
+    make_checkbox_pages(),
+    make_free_text_pages(c("name", "pub_ref", "location")),
+    personal_page_info(),
+    psychTestR::code_block(
+      function(state, ...){
+        psychTestR::set_local("repeat", TRUE, state)
+        psychTestR::set_local("owner_page_counter", 1, state)
+      }
+    ),
+    psychTestR::while_loop(test = function(state, ...) psychTestR::get_local("repeat", state),
+                           logic =
+                             make_person_page(label = "owner",
+                                              prompt = "Please enter the personal data for the dataset owner")
+    ),
+    psychTestR::code_block(
+      function(state, ...){
+        psychTestR::set_local("repeat", TRUE, state)
+        psychTestR::set_local("creator_page_counter", 1, state)
+      }
+    ),
+    psychTestR::while_loop(test = function(state, ...) psychTestR::get_local("repeat", state),
+                           logic =
+                             make_person_page(label = "creator",
+                                              prompt = "Please enter the personal data for the dataset creator")
+    ),
     make_free_text_pages(c("feedback")),
     
-    # psychTestR::reactive_page(function(state, ...){
-    #   res <- psychTestR::get_results(state, complete = T, add_session_info = T) %>% as.list()
-    #   browser()
-    # }),
+    psychTestR::reactive_page(function(state, ...){
+       res <- psychTestR::get_results(state, complete = T, add_session_info = T) %>% as.list()
+       browser()
+     }),
     psychTestR::elt_save_results_to_disk(complete = TRUE),
     psychTestR::final_page(shiny::p(shiny::h4("Thank you for participating!"),
                                     shiny::h4("You can close the browser tab now."),
---
+++ stimulatory_survey_resources.R
@@ -0,0 +1,207 @@
+intro_page <- function(){
+  psychTestR::one_button_page(
+    body = shiny::div(
+      shiny::h3("Dear colleagues,", style = "text-align:left"),
+      shiny::p("we like to gather some information on stimulus sets you were using in your past research."),
+      shiny::p("This is a pre-screening survey to get an impression on what kind of stimulus data sets are out there,", 
+               "Filling in the survey will help the Stimulus Database Working Group (SDB-WG) to set up a system", 
+               "allowing researcher the easy re-use of stimuli while serving as a standardized archive and documenting site for stimulus sets at the same time."),
+      shiny::p("You can also enter more than one stimulus set using the link at the end of survey"),
+      shiny::p("Best wishes,", shiny::tags$br(), 
+               "Klaus Frieler", shiny::tags$br(), 
+               "(on behalf of the SDB-WG)", style = "font-style:italic"),
+      style = "margin-left:20%;margin-right:20%;text-align: justify"
+      
+    ),
+    button_text = "Continue"
+  )
+}
+
+personal_page_info <- function(){
+  psychTestR::one_button_page(
+    body = shiny::div(
+      shiny::h3("Owner and creator information", style = "text-align:left"),
+      shiny::p("First, we ask for some  personal information of the creators and owners of the stimulus set"),
+      shiny::p("Typically, there are one or more persons involved with the creation of the stimulus set.",
+               "Likewise, the are might be one or more persons acting as the owner,",
+               "i.e., a person responsible for maintaining and curating the dataset."),
+      shiny::p("One the next pages, you should enter some basic information for the  creators and owners.",
+               "First and last name are mandatory. Additionaly, a professional role and the department of the MPIAE", 
+               "if applicable, should to be selected. ORCID is optional."),
+      shiny::p("Press 'Continue' to move on to the next section, press 'Another entry' to enter, surprise, 
+               another person's data."),
+      style = "margin-left:20%;margin-right:20%;text-align: justify"
+    ),
+    button_text = "Continue"
+  )
+}
+
+p_id_prompt <- shiny::div(
+  shiny::h4("Stimulus Set ID"),
+  shiny::p(
+  "Please enter an ID for your stimulus set, which may contain only letters, numbers, and underscores.",
+  "You can choose the ID freely, but it should be rather unique.",
+  "Your can use this ID also to get back to this survey, after an interruption from data entry.", 
+  "Once you finished data entry for a stimulus set, you cannot use the same ID again. If you need this really, please contact me."),
+  style = "margin-left:20%;margin-right:20%;text-align: justify"
+)
+professional_roles <- c(
+  "PhD",
+  "Post-Doc",
+  "Senior Researcher", 
+  "Research Group Leader",
+  "Director", 
+  "Other")
+
+problems_info = list(
+  de = shiny::tags$span("Probleme? Kontaktieren Sie klaus.frieler@ae.mpg.de mit einem Link zu dieser Seite.", 
+                        style = "color:#999999",
+  ),
+  de_f = shiny::tags$span("Probleme? Kontaktieren Sie klaus.frieler@ae.mpg.de mit einem Link zu dieser Seite.", 
+                          style = "color:#999999",  
+  ),
+  en = shiny::tags$span("Problems? Contact klaus.frieler@ae.mpg.de with a link to this page.", 
+                        style = "color:#999999",
+                        shiny::tags$br(),
+  )
+)
+
+likert_items <- c()
+
+free_text_items <- list(
+  name = list(
+    prompt = "Please give a succinct and descriptive name for your stimulus data set"
+  ),
+  pub_ref = list(
+    prompt = "Please enter the DOI for a reference publication, where the stimulus set was (first) used.",
+    subprompt = "If you don't have a DOI, please provide bibliographical information (in any style). You can also enter more than one DOI or citation, separated by blank lines."
+  ),
+  location = list(
+    prompt = "Please enter a URL of stimulus", 
+    subprompt = "Only if available. URL needs not  to be publicly available. URL is likely to change later"),
+  general_description = list(
+    prompt = "Please describe the stimulus data set shortly",
+    subprompt = shiny::p("Just the gist, general idea and concept and of the stimuli, areas of application.",
+                         style = "font-size:small;color:darkblue;text-align:justify;margin-left:20%;margin-right:20%;width:600px")
+  ),
+  design_spec = list(
+    prompt = "Please describe the design of the stimulus",
+    subprompt = shiny::p("The design of a stimulus set pertains to the logical axes of systematic manipulations or conditions used for constructing the stimulus set.",
+                         "For example, a set of texts consisting three different text types (poems, proverbs, Busch epithets),", 
+                         "which are modified each to four different versions (original, no rhyme, no meter, no rhyme and no meter),",
+                         " which are avaible in two different media formats (written text and audio)",
+                         style = "font-size:small;color:darkblue;text-align:justify;margin-left:20%;margin-right:20%;width:600px")
+  ),
+  naming_scheme = list(
+    prompt = "Please describe the naming scheme",
+    subprompt = shiny::p("For other to make use of your stimulus set, it is vital to understand the naming scheme of the files. 
+                         We recommend be be very consistent here, to use only small letters and underscores and to reflect the general design clearly in the names.",
+                         "If the dataset does not contain huge amounts of files, it mighte be advisable to use a maximally flat (shallow) folder structure",
+                         "with an expressive and concise naming scheme instead of hierarchies of nested folders. No two files",
+                         "should have the same name (including the file extension) while  being discriminated by their containing folders. If your stimulus set",
+                         "does not follow these guidelines yet, we suggest considering to change the file organisiation accordingly before you submit your stimuli.",
+                         style = "font-size:small;color:darkblue;text-align:justify;margin-left:20%;margin-right:20%;width:600px")
+  ),
+  reference_set = list(
+    prompt = "Please enter a reference ID",
+    subprompt = "If your stimulus set is connected to another stimulus set, than you can enter the ID here."
+  ),
+  feedback = list(
+    prompt = "Would you like to give some feedback on the survey or on the project itself?"
+  )
+)
+
+numeric_input_items <- list(
+  subsets = list(
+    prompt = "How many proper subsets does the stimulus set contain?"
+  ),
+  entities = list(
+    prompt = "How many files does the stimulus set contain?"
+  )
+)
+
+dropbox_items <- list(
+  source = list(
+    prompt = "What is the origin of the main material in the stimulus set?",
+    subprompt = shiny::tags$ul( 
+      shiny::tags$li("'External' means that the stimuli are based on foreign source material"), 
+      shiny::tags$li("'Commercial' means that the source material was bought from a third party, which holds the copyrights"),
+      shiny::tags$li("'Free' means that the source material was free to use (e.g., CC license)"),
+      shiny::tags$li("'Experiment' means that stimuli were created in a production experiment either by a third party (External) or by the creators (Internal)."),
+      shiny::tags$li("'Internal' means that the stimuli were self-created"),
+      shiny::tags$li("'Mixed' means that the stimulus set contains a mixture of the above"),
+      shiny::tags$li("'Other' means a currently unimaginable different way of stimulus creation"),
+      style = "font-size:small;text-align:justify;margin-left:20%;margin-right:20%;width:60%"),
+    items = c("External/Commercial", 
+              "External/Free",
+              "External/Experiment", 
+              "Internal/Researcher",
+              "Internal/Experiment", 
+              "Mixed", 
+              "Other")),
+  
+  modification = list(
+    prompt = "For external material, how many modifications have been made?",
+    items = c("None", "Some", "Substantial", "Mixed", "Only internal material" )
+  )
+  # ,
+  # role = list(
+  #   prompt= "What is your professional role?", 
+  #   items = c(
+  #     "PhD",
+  #     "Post-Doc",
+  #     "Senior Researcher", 
+  #     "Research Group Leader",
+  #     "Director", 
+  #     "Other"))
+)
+
+checkbox_items <- list(
+  access_level = list(
+    prompt = "Please select all access levels that apply for the different parts of the stimulus set (as of today).",
+    subprompt = "",
+    items = c("Fully restricted",
+              "Internal use (MPIAE)",
+              "Restricted (Conditional use)",
+              "Upon request only",
+              "open access")
+  ),
+  scientific_field = list(
+    prompt = "In which scientific fields  was the stimulus set used so far?",
+    subprompt = "Multiple answers possible",
+    items = c("Psychology", "Neuroscience", "Musicology",
+              "Literary science", "(Psycho-)Linguistics",
+              "Media science", "Sociology", "Genetics", 
+              "Other")
+  ),
+  psych_topic = list(
+    prompt = "For which psychological or neuroscience topic was the stimulus set used so far?",
+    subprompt = "Multiple answers possible",
+    items = c("Non-psychological", 
+              "Aesthetics", "Emotion", "Perception", "Cognition", "Memory", "Production", "Physiology", 
+              "Brain morphology",
+              "Other")
+  ),
+  art_domain = list(
+    prompt = "To which aesthetic or artistic domain does you stimulus set main pertain?",
+    subprompt = "Multiple answers possible",
+    items = c("Non-aesthetics", "Music", "Literature", 
+              "Non-fiction Text", "Visual Arts", 
+              "Movies", "Music Videos", "Dance", "Theatre", 
+              "Performance", "Other")
+  ),
+  paradigm = list(
+    prompt = "For which experimental paradigm was the stimulus set mainly designed and/or used for?",
+    subprompt = "Multiple answers possible",
+    items = c("Oddball", "Comparison", "Reaction time", "Judgement/Rating", "fMRI", "EEG", "Eye-tracking", "MEG", 
+              "tDCM", "Physiology", "Other")
+  ),
+  media_types = list(
+    prompt = "Please select all media types contained in the stimulus set",
+    items = c("Text","Picture", "Video/Live", "Video/Animated", "Audio/Music", "Audio/Speech",
+              "Audio/Sounds", "Audio/Mixed", "VR/AR", "Other")
+  )
+  
+)
+
+likert_options <- c("Strongly disagree", "Disagree", "Neither disagree nor agree", "Agree", "Strongly agree") 
